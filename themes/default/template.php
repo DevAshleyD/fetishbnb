2216d0893ebdd3e03ed6e1d6c0a8ad5d69f63254
Rob Reyes <rooob.reyes@gmail.com>
2018-09-05 23:38:55 +0800

    Removed batches from user menu

--- themes/default/template.php
+++ themes/default/template.php
@@ -240,7 +240,6 @@
                                                 <li><a href="<?php echo site_url('admin'); ?>"><?php echo lang('menu_admin'); ?></a></li>
                                                 <?php } ?>
                                                 <li><a href="<?php echo site_url('/profile'); ?>"><?php echo lang('action_profile'); ?></a></li>
-                                                <li><a href="<?php echo site_url('mybatches') ?>"><?php echo lang('menu_my_batches') ?></a></li>
                                                 <li><a href="<?php echo site_url('myevents') ?>"><?php echo lang('menu_my_events') ?></a></li>
                                                 <li><a href="<?php echo site_url('logout'); ?>"><?php echo lang('action_logout') ?></a></li>
                                             </ul>
