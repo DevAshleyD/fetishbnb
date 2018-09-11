d2d58fab254db3db83e66821adb4489f96849fc3
Rob Reyes <rooob.reyes@gmail.com>
2018-09-11 18:13:32 +0800

    Model for BTC transaction

---
+++ application/models/Btc_model.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * BTC Transaction Model
+ *
+ * This model handles btc_transaction module data
+ *
+ * @package     eastvantage
+ * @author      rob reyes
+*/
+
+class Btc_model extends CI_Model {
+
+    /**
+     * Constructor
+     */
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @vars
+     */
+    private $table = 'btc_transaction';
+
+    public function count_transactions()
+    {
+        return $this->db->count_all_results($this->table);
+    }
+
+    public function get_user_transaction($id = NULL)
+    {
+        return $this->db->select(array('id','user_id','event_id','amount','date'))
+                         ->where(array('user_id'=>$id))
+                         ->get($this->table)
+                         ->result_array();
+
+    }
+}
