70688cb8db2b147a2bebfd6fd9b1341e651913dd
Robin Reyes <rooob.reyes@gmail.com>
2018-10-02 22:19:15 +0800

    Removed user details when doing a booking

--- application/views/e_booking.php
+++ application/views/e_booking.php
@@ -4,127 +4,8 @@
 	<div class="container" id="booking-page">
 
 		<div class="row shop-forms">
-
+			<input type="hidden" name="logged_in" value="<?php echo $this->session->userdata('logged_in') ? 1 : ''; ?>">
 			<!-- User Login Tab -->
-			<div class="col-sm-12">
-				<div class="content-box shadow bg-lgrey">
-					<input type="hidden" name="logged_in" value="<?php echo $this->session->userdata('logged_in') ? 1 : ''; ?>">
-					<?php if ($this->session->userdata('logged_in')) { ?>
-					<div class="row">
-						<div class="col-md-12">
-							<h4><?php echo lang('e_l_logged_in_user') ?></h4>
-						</div>
-					</div>
-					<div class="row">
-					  	<div class="col-md-4 col-md-offset-1 image-card">
-					     	<div class="picture-container">
-					          	<div class="picture">
-					              	<img src="<?php echo $this->user['image'] ? base_url('upload/users/images/').$this->user['image'] : base_url('themes/default/img/avatar.jpg'); ?>" class="picture-src img-responsive" id="wizardPicturePreview" title=""/>
-					          	</div>
-					      	</div>
-					  	</div>
-					  	<div class="col-md-6">
-					  	    <table cellspacing="0" class="shop_table cart">
-					  	    	<tr>
-					  	    		<td><?php echo lang('users_name') ?></td>
-					  	    		<td class="text-capitalize"><?php echo $this->user['first_name'].' '.$this->user['last_name'] ?></td>
-					  	    	</tr>
-					  	    	<tr>
-					  	    		<td><?php echo lang('users_email') ?></td>
-					  	    		<td><?php echo $this->user['email']; ?></td>
-					  	    	</tr>
-					  	    	<tr>
-					  	    		<td><?php echo lang('users_mobile') ?></td>
-					  	    		<td><?php echo $this->user['mobile']; ?></td>
-					  	    	</tr>
-					  	    	<tr>
-					  	    		<td><?php echo lang('users_address') ?></td>
-					  	    		<td class="text-capitalize"><?php echo $this->user['address']; ?></td>
-					  	    	</tr>
-					  	    </table>
-					  	</div>
-					</div>
-					<?php } else {
-				  	// get current uri
-						$_SESSION['redirect_url'] = "/" . uri_string();  ?>
-						<div class="row">
-						<div class="col-md-6">
-							<div class="signin-form">
-									<?php echo form_open('auth/login', array('class'=>'')); ?>
-											<div class="row">
-													<div class="form-group <?php echo form_error('identity') ? ' has-error' : ''; ?>">
-															<div class="col-md-12">
-																	<label for="identity">Please login</label>
-																	<?php echo form_input(array('name'=>'identity', 'id'=>'identity', 'class'=>'form-control', 'placeholder'=>lang('users_email_username'))); ?>
-															</div>
-													</div>
-											</div>
-											<div class="row">
-													<div class="form-group <?php echo form_error('password') ? ' has-error' : ''; ?>">
-															<div class="col-md-12">
-																	<?php echo form_password(array('name'=>'password', 'id'=>'password', 'class'=>'form-control', 'placeholder'=>lang('users_password'), 'autocomplete'=>'off')); ?>
-															</div>
-													</div>
-											</div>
-											<div class="row">
-													<div class="col-md-6">
-															<span class="remember-box checkbox">
-																 <label for="remember">
-																	<input type="checkbox" id="remember" name="remember" value="1"><?php echo lang('login_remember_label') ?>
-																</label>
-															</span>
-													</div>
-													<div class="col-md-6">
-														<span class="remember-pass pull-right">
-															<a class="pull-right a-hover" href="<?php echo site_url('auth/forgot_password'); ?>"><?php echo lang('users_forgot'); ?></a>
-														</span>
-													</div>
-											</div>
-											<div class="row">
-													<div class="col-sm-4">
-															<!-- Button -->
-															<?php echo form_button(array('type' => 'submit', 'class' => 'btn', 'data-loading-text'=>lang('action_loading'), 'content' => lang('users_login'))); ?>
-													</div>
-											</div>
-									<?php echo form_close(); ?> <!-- Form Ends -->
-							</div>
-					</div>
-					<div class="col-md-6">
-						<div class="signin-form">
-						<?php echo form_open_multipart('auth/register', array('role'=>'form', 'class'=>'', 'id'=>'form_login')); ?>
-						<div class="row">
-							<div class="form-group <?php echo form_error('email') ? ' has-error' : ''; ?>">
-								<label for="email">or register to proceed</label>
-											<?php echo form_input(array('name'=>'email', 'value'=>set_value('email', (isset($user['email']) ? $user['email'] : '')), 'class'=>'form-control', 'type'=>'email', 'placeholder'=>lang('users_email'))); ?>
-							</div>
-						</div>
-						<div class="row">
-							<div class="form-group <?php echo form_error('username') ? ' has-error' : ''; ?>">
-											<?php echo form_input(array('name'=>'username', 'value'=>set_value('username', (isset($user['username']) ? $user['username'] : '')), 'class'=>'form-control','placeholder'=>lang('users_username'))); ?>
-							</div>
-						</div>
-						<div class="row">
-							<div class="form-group <?php echo form_error('password') ? ' has-error' : ''; ?>">
-									<?php echo form_password(array('name'=>'password', 'value'=>set_value('password', (isset($user['password']) ? $user['password'] : '')), 'class'=>'form-control', 'placeholder'=>lang('users_password'))); ?>
-							</div>
-						</div>
-						<div class="row">
-							<div class="form-group <?php echo form_error('password_confirm') ? ' has-error' : ''; ?>">
-										<?php echo form_password(array('name'=>'password_confirm', 'value'=>'', 'class'=>'form-control', 'autocomplete'=>'off','placeholder'=>lang('users_password_confirm'))); ?>
-							</div>
-						</div>
-						<div class="row">
-		            <div class="col-sm-4 pull-right padding-0">
-		                <button type="submit" name="submit_form" id="submit_form" class="btn pull-right"><?php echo lang('users_register'); ?></button>
-		            </div>
-		        </div>
-						<?php echo form_close(); ?>
-					</div>
-				</div>
-				</div>
-					<?php } ?>
-				</div>
-			</div>
 			<!-- End User Login Tab -->
 
 			<!-- Select Batch Tab -->
@@ -304,7 +185,7 @@
 
 				</div>
 			</div>
-			<?php } ?>
+		<?php }else{ redirect(site_url('auth')); }?>
 
 		</div><!-- Row -->
 
